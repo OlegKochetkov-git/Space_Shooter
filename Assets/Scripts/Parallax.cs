8c48fae5b36ba6a8c8d889979b4bb04a3839d33c
Oleg Kochetkov <olegkochetkov1@gmail.com>
2021-09-15 04:47:37 +0500

    Fix parallax effect

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -5,8 +5,41 @@ using UnityEngine.UI;
 
 public class Parallax : MonoBehaviour
 {
-    [SerializeField] RawImage img;
-    [SerializeField] float speed;
+    static Parallax instance;
+    [SerializeField] List<RawImage> planet1;
+    [SerializeField] float speed1;
+    //[SerializeField] RawImage planet2;
+    [SerializeField] float speed2;
+
+    //[SerializeField] RawImage planet3;
+    [SerializeField] float speed3;
+
+
+    void Awake()
+    {
+        //int number = FindObjectsOfType<Parallax>().Length;
+        //if (number > 1)
+        //{
+        //    gameObject.SetActive(false);
+        //    Destroy(gameObject);
+        //}
+        //else
+        //{
+
+        //DontDestroyOnLoad(gameObject);
+        //}
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+
+    }
 
     void Update()
     {
@@ -15,8 +48,13 @@ public class Parallax : MonoBehaviour
 
     void MoveImage()
     {
-        Rect rect = img.uvRect;
-        rect.x += speed * Time.deltaTime;
-        img.uvRect = rect;
+        foreach (RawImage item in planet1)
+        {
+            
+            Rect rect = item.uvRect;
+            rect.x += speed1 * Time.deltaTime;
+            item.uvRect = rect;
+        }
+        
     }
 }
