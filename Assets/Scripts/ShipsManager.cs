6fb20eb23949ad9c8362faebf197852b021a5fab
Oleg Kochetkov <olegkochetkov1@gmail.com>
2021-09-16 11:46:21 +0500

    save place

---
+++ Assets/Scripts/ShipsManager.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipsManager : MonoBehaviour
+{
+    [SerializeField] List<PlayerShipConfig> ships;
+}
